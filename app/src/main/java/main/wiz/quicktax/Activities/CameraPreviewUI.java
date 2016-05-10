2670f7c96a28f14580883e1a67dd73de18b03d78
Shanyl Jimenez <falsenoglory@gmail.com>
2016-05-10 12:42:45 +0800

    added camera preview activity

---
+++ app/src/main/java/main/wiz/quicktax/Activities/CameraPreviewUI.java
@@ -0,0 +1,13 @@
+package main.wiz.quicktax.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CameraPreviewUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_preview_ui);
+    }
+}
